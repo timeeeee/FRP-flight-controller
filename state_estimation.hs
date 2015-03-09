e443c85b42813214127a697f4e41e19109e9bf4d
angie-ku-edu <angie.ku.edu@gmail.com>
2015-03-09 08:54:20 -0500

    Revert "Warning - Not working!"
    This reverts commit 81ef3c7d05c20c5a59cadeec801e335de1deeb4d.

--- state_estimation.hs
+++ state_estimation.hs
@@ -1,12 +1,6 @@
 module State_Estimation  
 (stateOutput) where  
 
-import Common_Equations
-
-
-
 --Engine Constants
-stateOutput :: [Float] -> [Float] -> [Float]
-stateOutput xs [] = []
-stateOutput xs ys = ys
-										
+stateOutput :: [Float] -> [Float]
+stateOutput xs = xs
