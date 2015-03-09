aa5bb0402d26f51e3f7f8753960e69cb6e8747cb
angie-ku-edu <angie.ku.edu@gmail.com>
2015-03-09 09:23:59 -0500

    Passes Previous Result Back
    Finally!  This passes the previous result back through the mainloop
    function  - I still don't understand what was going wrong but it had
    something to do with trying to perform an assignment before passing it
    back.

--- main.hs
+++ main.hs
@@ -11,34 +11,45 @@ main :: IO ()
 main = do 
        inh <- openFile "input.csv" ReadMode
        outh <- openFile "output.txt" WriteMode
-       mainloop inh outh
+       mainloop inh outh []
        hClose inh
        hClose outh
 
-mainloop :: Handle -> Handle -> IO ()
-mainloop inh outh = 
+
+mainloop :: Handle -> Handle -> [Float] -> IO ()
+mainloop inh outh [] = 
     do ineof <- hIsEOF inh
        if ineof
            then return ()
            else do 
+								--get samples from file
 								inp <- hGetLine inh
-								print (getState (state inp))
-								mainloop inh outh
+								--print to screen the result of getState
+								mainloop inh outh (getState (state inp))
+mainloop inh outh prev = 
+    do ineof <- hIsEOF inh
+       if ineof
+           then return ()
+           else do 
+								--get samples from file
+								inp <- hGetLine inh
+								--print to screen the result of getState
+								print prev
+								mainloop inh outh (getState (state inp))
 
 
-									 
-									 
+--helper functions to parse string from file in mainloop			
 
-
---helper functions to parse string from file in mainloop											 
+--turn the string into a list of floats								 
 getState :: [String] -> [Float]
-getState xs = let [phi,theta, psi, p, q, r, lat, lon, alt, vnorth, veast, vdown, rc_throttle, rc_ele, rc_ail, rc_rudder, vt, alpha, beta] = map read xs
-	    in stateOutput [phi,theta, psi, p, q, r, lat, lon, alt, vnorth, veast, vdown, rc_throttle, rc_ele, rc_ail, rc_rudder, vt, alpha, beta]
-
+getState xs = map read xs
+	    
+--turn the string into a list of string items
 state :: String -> [String]
 state xs = (take 19  (sample xs))
 sample xs = 	split ',' xs
 						
+--split the string						
 split :: Eq a => a -> [a] -> [[a]]
 split d [] = []
 split d s = x : split d (drop 1 y) where (x,y) = span (/= d) s
