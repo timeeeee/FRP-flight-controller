bc1b931da155c4359940143ca750e429c4489249
angie-ku-edu <angie.ku.edu@gmail.com>
2015-03-11 01:26:44 -0500

    Added more functions
    Added more functions - committing to be safe in working product

--- state_estimation.hs
+++ state_estimation.hs
@@ -6,8 +6,8 @@ import Constants
 
 
 stateOutput :: [Float] -> [[Float]] -> [[Float]]
-stateOutput xs [] = [stateObserver xs [],	navigationObserver xs []]
-stateOutput xs ys = [stateObserver xs (ys !! 0),	navigationObserver xs (ys !! 1)]
+stateOutput xs [] = [stateObserver xs [] [],	navigationObserver xs [] [], xs]
+stateOutput xs ys = [stateObserver xs (ys !! 0) (ys !! 2),	navigationObserver xs (ys !! 1) (ys !! 2), xs ]
 
 
 -- stateObserver INPUT: 
@@ -25,31 +25,55 @@ stateOutput xs ys = [stateObserver xs (ys !! 0),	navigationObserver xs (ys !! 1)
 
 
 -- PART ONE - STATE OBSERVER
-stateObserver :: [Float] -> [Float] -> [Float]
-stateObserver xs [] = -- gforce to fps, then into ekf
-											gainBias(ekf (gainAcc xs) [])
-stateObserver xs ys = gainBias(ekf (gainAcc xs) ys)
+-- Note: xs is the current measurement,  ys is previous observer output, zs is the k-1 measurement
+stateObserver :: [Float] -> [Float] -> [Float] -> [Float]
+stateObserver xs [] []  = -- gforce to fps, then into ekf
+											gainBias(ekf (gainAcc xs) [] [])
+stateObserver xs ys zs = gainBias(ekf (gainAcc xs) ys zs)
 												
 												
 -- helper functions for stateObserver
-ekf :: [Float] -> [Float] -> [Float]	
-ekf xs [] = xs	
-ekf xs ys = xs
+-- helper functions for stateObserver
+ekf :: [Float] -> [Float] -> [Float] -> [Float]	
+ekf xs [] [] = (let (beginX, endX) = (splitAt 9 xs)
+								in (y_ beginX) ++ (x_est_ endX []) ++ (k_function endX []))
+ekf xs ys zs = let ((beginX, endX),(beginZ, endZ)) = ((splitAt 9 xs),(splitAt 9 zs))
+								in (subElem (y_ beginX) (y_est_ (x_est_ endZ ys))) ++ (k_function endZ ys)
+						
 
+
+subElem :: [Float] -> [Float] -> [Float]
+subElem xs ys = (map sub1 (zip xs ys))
+
+sub1 :: (Float,Float) -> Float
+sub1 x = (fst x) - (snd x)
+						
 gainAcc :: [Float] -> [Float]
 gainAcc xs = --split at acc and rebuild
 							let (begin,end) = splitAt 9 xs   in begin ++ [(g + (end !! 0))] ++ [g + (end !! 1)] ++ [g + (end !! 2)] ++ (drop 3 end)
 
 gainBias :: [Float] -> [Float]
-gainBias xs = --split at acc and rebuild
+gainBias xs = --split at bias and rebuild
 							let (begin,end) = splitAt 17 xs   in begin ++ [1 + (end !! 0)] ++ [1 + (end !! 1)]
 							
+y_ :: [Float] -> [Float]
+y_ xs = xs 
 
+y_est_ :: [Float] -> [Float]
+y_est_ xs = xs
+
+x_est_ :: [Float] -> [Float] -> [Float]
+x_est_ xs [] = xs
+x_est_ xs ys = xs
+
+k_function :: [Float] -> [Float] -> [Float]
+k_function xs [] = xs
+k_function xs ys = xs
 							
 -- PART TWO - NAVIGATION OBSERVER							
-navigationObserver :: [Float] -> [Float] -> [Float]								
-navigationObserver xs [] = ekf xs []
-navigationObserver xs ys = ekf xs ys
+navigationObserver :: [Float] -> [Float] -> [Float]	-> [Float]						
+navigationObserver xs [] [] = ekf xs [] []
+navigationObserver xs ys zs = ekf xs ys zs
 
 
 
@@ -61,8 +85,8 @@ navigationObserver xs ys = ekf xs ys
 --input of h is output of f
 
 --
-y_ xs = xs 
 
-f xs ys = xs
+f :: [Float] -> [Float] -> Float -> [Float]
+f xs ys alphaDot_1 = xs
 
 h xs = xs
