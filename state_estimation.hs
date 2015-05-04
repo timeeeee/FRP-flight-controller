59911952e298b650e1587de085cc9dc731aa88d7
angie-ku-edu <angie.ku.edu@gmail.com>
2015-05-04 17:42:31 -0500

    going to revert to other push

--- state_estimation.hs
+++ state_estimation.hs
@@ -25,7 +25,9 @@ import Data.List
 
 stateOutput :: [Float] -> [[Float]] -> [[Float]]
 stateOutput xs [] = [stateObserver xs [] [],	navigationObserver xs [] [], xs]
-stateOutput xs ys = [stateObserver xs (rateLimit(ys !! 0)) (ys !! 2),	navigationObserver xs (ys !! 1) (ys !! 2), xs ]
+stateOutput xs ys = [stateObserver xs (rateLimit(ys !! 0)) (ys !! 2),	
+										 navigationObserver xs (ys !! 1) (ys !! 2), 
+										 xs]
 
 
 ----------------------------------------------------------------------------------
@@ -68,9 +70,7 @@ stateOutput xs ys = [stateObserver xs (rateLimit(ys !! 0)) (ys !! 2),	navigation
 --								biases_est													  (13,14)
 --								axb, ayb, azb, 												(15,16,17)
 --								alphaDot, alphaDot1										(18,19)
---[[-4.5309113e11,6.967565e11,5.4941586e17,5.2945036e18,-8.0631745e13,2.5494173e14,-2.6060903e20,8.162134e17,5.1490807e12,-7.6692084e17,-2.5837898e12,6.83236e18,-2.005717e14,-6.831078e13,6.783386e19,1.4865645,-2.5652207e-31,-7.292938e-5,0.0,1.4489848e16],
---[39.253124,-96.93884,529.4541,6.1339475e-2,-0.28544784,0.5771742,0.0,0.0,0.0],
---[42.70626,0.0,0.0,0.201245,-0.9365086,-1.8936161,22.775213,0.5654125,47.41833,0.4974103,1.9278824e-2,3.811761e-3,-2.505436e-3,1.4711692e-2,7.4e-5,-0.98087144,39.253124,-96.93884,529.4541,12.953088,-1.1230818,0.62852925,1.0]]
+
 
 -- Summary Note: 
 -- xs is the current measurement,  
@@ -81,19 +81,19 @@ stateObserver xs [] []  = (ekf (gainAcc xs) [] [])
 stateObserver xs ys zs = (ekf (gainAcc xs) ys zs)
 
 
-
 -- ** DONE adding gain to acceleration data to transform to g-force to fps2
 --split at acc and rebuild
 gainAcc :: [Float] -> [Float]
-gainAcc xs = (take 12 xs) ++ (accelerate (take 3 (drop 12 xs))) ++ (drop 15 xs)
---let (begin,end) = splitAt 13 xs   in begin ++ [(g * (end !! 0))] ++ [g * (end !! 1)] ++ [g * (end !! 2)] ++ (drop 3 end)
+gainAcc xs = (take 13 xs) ++ (accelerate (take 3 (drop 13 xs))) ++ (drop 16 xs)
+
 accelerate :: [Float] -> [Float]
 accelerate xs = [(g * (xs !! 0))] ++ [g * (xs !! 1)] ++ [g * (xs !! 2)]
 
 -- ** DONE adding gain to bias elements -- this is done to the final output of ekf
 --split at bias and rebuild
 gainBias :: [Float] -> [Float]
-gainBias xs = let (begin,end) = splitAt 13 xs   in begin ++ [1 * (end !! 0)] ++ [1 * (end !! 1)] ++ (drop 2 end)
+gainBias xs = (let (begin,end) = splitAt 13 xs   
+							 in begin ++ [1 * (end !! 0)] ++ [1 * (end !! 1)] ++ (drop 2 end))
 
 
 
@@ -108,9 +108,24 @@ gainBias xs = let (begin,end) = splitAt 13 xs   in begin ++ [1 * (end !! 0)] ++ 
 ekf :: [Float] -> [Float] -> [Float] -> [Float]	
 -- initial output of ekf for first iteration: NOTE** may want biases and alphadots intitialized to something other than zero **
 ekf xs [] [] = (let (vt, alpha, beta, p, q, r, hx_body, hy_body, hz_body, throttle_cmd, elevator_cmd, aileron_cmd, rudder_cmd, axb, ayb, azb) = ((xs !! 0), (xs !! 1),(xs !! 2),(xs !! 3),(xs !! 4),(xs !! 5),(xs !! 6),(xs !! 7),(xs !! 8),(xs !! 9),(xs !! 10),(xs !! 11),(xs !! 12),(xs !! 13),(xs !! 14),(xs !! 15))
-								in [throttle_cmd, elevator_cmd, aileron_cmd, rudder_cmd,vt, alpha, beta,hx_body, hy_body, hz_body,p, q, r,0.0,0.0,axb, ayb, azb,0.0,0.0])
+								in [throttle_cmd, elevator_cmd, aileron_cmd, rudder_cmd,
+										vt, alpha, beta,
+										hx_body, hy_body, hz_body,
+										p, q, r,
+										0.0,0.0,
+										axb, ayb, azb,
+										0.0,0.0])
 ekf xs ys zs = (let (one, two, three) = ((y_ xs), (x_est_ ys zs), (k_function ys zs))
-								in (vectorSum (reshape(matrixProduct (three) (transpose [(vectorMinus one (y_est_ two))]))) (take 15 two)) ++ (drop 15 two))
+								in ((rateLimit two) ++ (rateLimit (vectorSum (reshape (matrixProduct three (transpose [vectorMinus (take 9 xs) (y_est_ two)]))) (take 15 two) ) ++ (drop 15 two))))
+								--vectorSum (reshape(matrixProduct (three) (transpose [(vectorMinus one (y_est_ two))]))) (take 15 two)) ++ (drop 15 two) ++ one)
+--sample of two
+--  0.5182963,1.931924e-2,	3.8875504e-3,-3.188739e-3,466.1799, -0.9963662,1.1359025e8,-7.5850997, 6.5481033,-45.30768, -31717.617, 2511.1797,-31717.617,0.0,      0.0,         97.349396,2113.705, 1373.538,0.0, -43.90859
+--[0.21582651,1.3289034e-2,-1.0241674e-2,4.8055653e-3,200.0,1.064609,1.5707964,3.1415927,-11.871361,-1.5707964,15.707963,-10.471976,200.0,0.0,0.0,3768.0417,2853.4448,-6984.7983,0.0,24.425413
+
+--sample of combo
+-- -6.0162454,2.0107422,	 -210980.88,   -2045073.5,   2369.7786,-10.876247,1.0066524e8,-305915.88,-646.99725, 323598.28,-31746.654,-2636696.3,-31865.545,10.358159,-2.6202062e7,97.349396,2113.705, 1373.538,0.0, -43.90859
+-- sample of rateLimited
+-- 0.15,			0.2618,				-0.3491,		-0.1745,200.0,-1.5707964,1.5707964,-3.1415927,-3.1415927,1.5707964,-15.707963,-10.471976,-200.0,0.5235988,-0.5235988,8310.7705,2852.8403,7253.87,0.0,32.1303
 
 
 -- y_ INPUT:
